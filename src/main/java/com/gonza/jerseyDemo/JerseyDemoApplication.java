dfefc74eb6bd2003036ae53faa6dd855f83a07c5
Izquierdo Heras <goio@gft.com>
2020-02-05 10:45:49 +0100

    primera subida

---
+++ src/main/java/com/gonza/jerseyDemo/JerseyDemoApplication.java
@@ -0,0 +1,16 @@
+package com.gonza.jerseyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JerseyDemoApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		//SpringApplication.run(JerseyDemoApplication.class, args);
+		new JerseyDemoApplication().configure(new SpringApplicationBuilder(JerseyDemoApplication.class)).run(args);
+	}
+
+}
---
+++ src/test/java/com/gonza/jerseyDemo/JerseyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonza.jerseyDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
